9be1824d511689489ad64358168e9b84f265ed5b
Garry Sinclair (Delegate) <gsi@delegate.dk>
2017-03-09 10:27:28 +0100

    Added connection string to StoreDbContext

--- code-red-dk/Models/StoreDbContext.cs
+++ code-red-dk/Models/StoreDbContext.cs
@@ -8,6 +8,8 @@ namespace code_red_dk.Models
 {
     public class StoreDbContext : DbContext
     {
+        public StoreDbContext() : base("DefaultConnection") { }
+
         public System.Data.Entity.DbSet<code_red_dk.Models.Person> People { get; set; }
     }
 }
